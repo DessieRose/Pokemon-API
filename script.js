ab24a78c7b752297b21f3d1eaec3b1c19b6dae76
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2025-12-05 13:13:45 +0100

    Added & Updated: Pokémon type and energy icons
    Added new energy and type icon images, updated existing type icons, and introduced texture overlays for card backgrounds. Modified HTML and CSS to support new overlay and improved card styling. Updated script.js for type icon handling and minor logic changes.

--- script.js
+++ script.js
@@ -36,6 +36,27 @@ const habitatImages = {
     unknown: "components/images/habitats/default.jpg"  // fallback
 };
 
+// const typeIcons = {
+//     bug: "components/images/energy-icons/grass.png",
+//     dark: "components/images/energy-icons/darkness.png",
+//     dragon: "components/images/energy-icons/dragon.png",
+//     electric: "components/images/energy-icons/lightning.png",
+//     fairy: "components/images/energy-icons/fairy.png",
+//     fighting: "components/images/energy-icons/fighting.png",
+//     fire: "components/images/energy-icons/fire.png",
+//     flying: "components/images/energy-icons/",
+//     ghost: "components/images/energy-icons/.png",
+//     grass: "components/images/energy-icons/grass.png",
+//     ground: "components/images/energy-icons/fighting.png",
+//     ice: "components/images/energy-icons/water.png",
+//     normal: "components/images/energy-icons/colorless.png",
+//     poison: "components/images/energy-icons/.png",
+//     psychic: "components/images/energy-icons/psychic.png",
+//     rock: "components/images/energy-icons/fighting.png",
+//     steel: "components/images/energy-icons/metal.png",
+//     water: "components/images/energy-icons/water.png",
+// }
+
 const fetchPokemon = async () => {
     try {
         const id = getRandomId();
@@ -51,7 +72,9 @@ const fetchPokemon = async () => {
 
         // Type icon
         const typeName = pokemon.types[0].type.name;
-        const typeIcon = `./components/images/type-icons/${typeName}.png`; 
+        // const energies = pokemon.types.map(t => t.type.name);
+        const typeIcon = `components/images/type-icons/${typeName}.png`;
+        // const energyIcon = `./components/images/energy-icons${energyIcon}.png`; 
 
         // Pokemon name
         const pokemonName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
@@ -61,7 +84,8 @@ const fetchPokemon = async () => {
         const habitatImage = habitatImages[habitat] || habitatImages["unknown"];
 
         // Background color change depending on icon
-        const cardBackground = pokemonCard.style.backgroundColor = `${typeName}`;
+        const cardBackground = pokemonCard.style.backgroundColor = `var(--${typeName})`;
+       
         // const changeBackgroundColor = cardBackground
 
         // Top section (Name, HP, Type)
@@ -69,7 +93,7 @@ const fetchPokemon = async () => {
             <p class="poke-name">${pokemonName}</p>
             <div class="top-div-icon">
                 <p class="poke-hp">${pokemon.stats.find(s => s.stat.name === "hp").base_stat} HP</p>
-                <img src="${typeIcon}" alt="${typeName} icon" class="type-icon">
+                <img class="energy-types" src="${typeIcon}" alt="${typeName} icon">
             </div>
         `;
 
@@ -89,7 +113,7 @@ const fetchPokemon = async () => {
 
         // Bottom Section (Abilities)
         const abilityList = pokemon.abilities
-            .map(a => `<li>${a.ability.name}</li>`)
+            .map(a => `<li>${a.ability.name > 2}</li>`)
             .join("");
 
         // Fetch ability details
